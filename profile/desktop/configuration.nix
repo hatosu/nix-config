36bc98cbffd69d559b8e619bdc4661b6e37e5482
hatosu <tylerjacobcox@tutamail.com>
2024-10-25 12:48:37 -0700

    -

--- profile/desktop/configuration.nix
+++ profile/desktop/configuration.nix
@@ -2,9 +2,10 @@
   
   imports = [
 
-    # import hardware.nix, package.nix, and home.nix
+    # import hardware.nix, package.nix, manage.nix, and home.nix
     ./hardware-configuration.nix
     ./package.nix
+    ./manage.nix
     inputs.home-manager.nixosModules.default
 
     # import modules
@@ -34,41 +35,6 @@
       inputs.self.overlays.stable-packages
     
     ];
-  };
-
-  nix = let flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs; in { settings = {
-
-      # enable experimental features
-      experimental-features = [ "nix-command" "flakes" ];
-
-      # disable global registry
-      flake-registry = "";
-
-      # workaround for https://github.com/NixOS/nix/issues/9574
-      nix-path = config.nix.nixPath;
-    
-    };
-
-    # auto-collect nix garbage
-    gc = {
-      dates = "daily";
-      options = "--delete-older-than 1d";
-      automatic = true;
-    };
-
-    # auto-optimize /nix/store
-    optimise = {
-      dates = [ "daily" ];
-      automatic = true;
-    };
-
-    # disable channels
-    channel.enable = false;
-
-    # make flake registry and nix path match flake inputs
-    registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
-    nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
-
   };
 
   # define hostname
---
+++ profile/desktop/manage.nix
@@ -0,0 +1,43 @@
+{ lib, config, pkgs, inputs, ... }: {
+
+  nix = let flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs; in { settings = {
+
+      # enable experimental features
+      experimental-features = [ "nix-command" "flakes" ];
+
+      # disable global registry
+      flake-registry = "";
+
+      # workaround for https://github.com/NixOS/nix/issues/9574
+      nix-path = config.nix.nixPath;
+
+      # custom binary cache
+      trusted-substituters = [ "https://cache.nixos.org" ];
+      extra-trusted-substituters = [ "https://nix-community.cachix.org" "https://nix-gaming.cachix.org" ];
+      extra-trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4=" ];
+    
+    };
+
+    # auto-collect nix garbage
+    gc = {
+      dates = "daily";
+      options = "--delete-older-than 1d";
+      automatic = true;
+    };
+
+    # auto-optimize /nix/store
+    optimise = {
+      dates = [ "daily" ];
+      automatic = true;
+    };
+
+    # disable channels
+    channel.enable = false;
+
+    # make flake registry and nix path match flake inputs
+    registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
+    nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
+
+  };
+
+}
