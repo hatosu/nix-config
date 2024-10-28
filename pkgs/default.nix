7a6661025875bf3f111570bd7468dd26a8577e0e
Hatosu <tylerjacobcox@tutamail.com>
2024-10-27 21:10:34 -0700

    Delete pkgs directory

--- pkgs/default.nix
+++
@@ -1,8 +0,0 @@
-pkgs: {
-
-  nixfind      = pkgs.callPackage ./nixfind {};
-  help         = pkgs.callPackage ./help {};
-  nixocr       = pkgs.callPackage ./nixocr {};
-  wallpaper    = pkgs.callPackage ./wallpaper {};
-
-}
