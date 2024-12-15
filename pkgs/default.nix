25b4b3b853cd622a03967adc2a545ecd1522be50
Hatosu <tylerjacobcox@tutamail.com>
2024-12-15 01:00:22 -0800

    Update default.nix

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,9 +1,11 @@
 pkgs: {
+
   # utils
   nixfind = pkgs.callPackage ./nixfind {};
   nixpaper = pkgs.callPackage ./nixpaper {};
   nixocr = pkgs.callPackage ./nixocr {};
   help = pkgs.callPackage ./help {};
+  install = pkgs.callPackage ./install {};
 
   # games
   wiiudownloader = pkgs.callPackage ./wiiudownloader {};
